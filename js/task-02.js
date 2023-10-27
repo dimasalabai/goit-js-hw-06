87ef87222b518929628c5957068b563eaa26e78e
Dima <dimasalabay0711@gmail.com>
2023-10-27 22:11:45 +0300

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+function createMarkup(arr) {
+	const listIng = document.getElementById("ingredients");
+	const arrLi = arr.map((ing) => {
+		const listItems = document.createElement("li");
+		listItems.textContent = ing;
+		listItems.classList.add("item");
+
+		return listItems;
+	});
+	listIng.append(...arrLi);
+	return;
+}
+
+createMarkup(ingredients);

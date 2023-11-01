4b0605654393c51e72fa7e1dca39ed8cb1d6ef61
Dima <dimasalabay0711@gmail.com>
2023-11-01 22:52:27 +0200

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+const userInfo = {};
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const {
+		elements: { password, email },
+	} = event.currentTarget;
 
+	if (email.value === "" || password.value === "") {
+		alert("Всі поля повинні бути заповнені.");
+		return;
+	}
+	userInfo[password.name] = password.value;
+	userInfo[email.name] = email.value;
+
+	form.reset();
+	console.log(userInfo);
+});
